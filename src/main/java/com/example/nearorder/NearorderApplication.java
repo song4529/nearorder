cb431c95debc5635a9f52d58951c53038d69a349
song4529 <gusdn4529@gmail.com>
2024-03-11 12:13:24 +0900

    first commit

---
+++ src/main/java/com/example/nearorder/NearorderApplication.java
@@ -0,0 +1,13 @@
+package com.example.nearorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NearorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NearorderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nearorder/NearorderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nearorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NearorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
